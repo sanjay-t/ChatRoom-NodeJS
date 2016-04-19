69a51e5192ebe15f9d15d02bba4dd0252ea68ff9
Sanjay Thimmarayappa <sanjayboygenius@gmail.com>
2016-04-19 15:28:40 -0700

    ChatRoom-NodeJS

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+﻿var socket = io();
+$('#send-message-btn').click(function () {
+    var d = new Date();
+    time = String(d.getHours()) +" : "+ String(d.getMinutes());
+    var msg = $('#message-box').val();
+    socket.emit('chat', msg)
+    $('#adder ul').append(
+        $('<li>').attr('class', 'mar-btm').append(
+            $('<div>').attr('class', 'media-left').append(
+                $('<img>').attr('src', 'http://bootdey.com/img/Content/avatar/avatar1.png').attr('class', 'img-circle img-sm').attr('alt', 'Profile Picture')
+            )).append(
+            $('<div>').attr('class', 'media-body pad-hor').append(
+                $('<div>').attr('class', 'speech').append(
+                    $('<a>').attr('href', '#').attr('class', 'media-heading').text("User")).append(
+                    $('<p>').text(msg))
+            )));
+    $('#message-box').val('');
+    return false;
+});
+socket.on('chat', function (msg) {
+    var d = new Date();
+    time = String(d.getHours()) +" : "+ String(d.getMinutes());
+    $('#adder ul').append(
+        $('<li>').attr('class', 'mar-btm').append(
+            $('<div>').attr('class', 'media-left').append(
+                $('<img>').attr('src', 'http://bootdey.com/img/Content/avatar/avatar1.png').attr('class', 'img-circle img-sm').attr('alt', 'Profile Picture')
+            )).append(
+            $('<div>').attr('class', 'media-body pad-hor').append(
+                $('<div>').attr('class', 'speech').append(
+                    $('<a>').attr('href', '#').attr('class', 'media-heading').text("User")).append(
+                    $('<p>').text(msg))
+            )));
+});
